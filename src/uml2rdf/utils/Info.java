f73deb45c0500924ee8b1fefad80e64a8facdb9c
MOREEWS Francois <francois.moreews@irisa.fr>
2019-05-23 11:35:51 +0200

    description annotation and controlled value management

---
+++ src/uml2rdf/utils/Info.java
@@ -0,0 +1,16 @@
+package uml2rdf.utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Info {
+
+	String description() default "" ;
+	String enumerate() default "" ;
+	
+
+}
